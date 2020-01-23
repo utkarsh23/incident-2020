aac9b906706e03b0c078970d040bc9fca4cb667a
Sujan242 <48852710+Sujan242@users.noreply.github.com>
2020-01-23 23:35:05 +0530

    Updated index.js and added styles

--- src/Icare/index.js
+++ src/Icare/index.js
@@ -3,11 +3,61 @@ import { Link } from 'react-router-dom';
 
 function Icare() {
   return (
-    <div>
-      <h1>i-Care Page</h1>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+    <div className="Icare">
+      <div className="iContainer">
+        <div class="abc" />
+
+        <h1>
+          <i>i-CARE</i>
+        </h1>
+
+        <center>
+          <img
+            className="I-care2"
+            src="http://incident.nitk.ac.in/assets/images/icare/icare_intro_1.jpg"
+            width="100%"
+          />
+        </center>
+
+        <div className="iCard-left">
+          <div className="iPara">
+            {" "}
+            A lot of children belonging to the fishing community in Mangalore
+            dropout of school to help their parents in their daily chores.
+            <br />
+            Beach school tries to reduce this student drop out by instilling in
+            them, an interest towards practical science, mathematics &
+            astronomy.
+            <br />
+            <br />
+            <br />
+          </div>
+          <div>
+            <img
+              className="I-care1"
+              src=" http://incident.nitk.ac.in/assets/images/icare/icare_intro.jpg"
+            />
+          </div>
+        </div>
+
+        <div className="iCard-right">
+          <div className="iPara">
+            A lot of children belonging to the fishing community in Mangalore
+            dropout of school to help their parents in their daily chores.
+            <br />
+            Beach school tries to reduce this student drop out by instilling in
+            them, an interest towards practical science, mathematics &
+            astronomy.
+          </div>
+          <div>
+            {" "}
+            <img
+              className="I-care1"
+              src=" http://incident.nitk.ac.in/assets/images/icare/icare_intro.jpg"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
