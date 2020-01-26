05e62c26a2ea23875e75169050497157752383ae
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-01-26 11:01:43 +0530

    About Us Section

--- src/About/index.js
+++ src/About/index.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import Lighthouse from '../static/Lighthouse.png';
+import AboutContent from './AboutContent.json';
+import './style.css'
 import { Link } from 'react-router-dom';
 
-function About() {
-  return (
-    <div>
-      <h1>About Us Page</h1>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-    </div>
-  );
+class About extends React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+  
+      }
+    }
+    render(){
+      return (
+        <div>
+          <img src={Lighthouse}></img>
+          <div class="background"></div>
+          <div class="hero">
+            <h1>
+              {AboutContent.title}
+            </h1>
+            <p>
+              {AboutContent.paragraph1}          
+            </p>
+            <p>
+              {AboutContent.paragraph2}
+            </p>
+            <p>
+              {AboutContent.paragraph3}
+            </p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+      );
+    }  
 }
-
+    
+  
 export default About;
+
+
+// <div>
+//   <h1>About Us Page</h1>
+//   <Link to="/">
+//     <button>Home</button>
+//   </Link>
+// </div>
+  
+
