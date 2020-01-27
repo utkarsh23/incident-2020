15c0ab3cca1e7179a7a655c74ad08b693fba9944
Utkarsh Patil <utkarsh2305@gmail.com>
2020-01-27 19:02:03 +0530

    About Us clean up.

--- src/About/index.js
+++ src/About/index.js
@@ -2,50 +2,34 @@ import React from 'react';
 import Lighthouse from '../static/Lighthouse.png';
 import AboutContent from './AboutContent.json';
 import './style.css'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class About extends React.Component {
-    constructor(props){
-      super(props);
-      this.state={
-  
-      }
-    }
-    render(){
-      return (
-        <div class="AboutBackground">
-          <img class="lighthouse" src={Lighthouse}></img>
-          <div class="background"></div>
-          <div class="hero">
-            <h1>
-              {AboutContent.title}
-            </h1>
-            <p>
-              {AboutContent.paragraph1}          
-            </p>
-            <p>
-              {AboutContent.paragraph2}
-            </p>
-            <p>
-              {AboutContent.paragraph3}
-            </p>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-          </div>
+  render() {
+    return (
+      <div class="AboutBackground">
+        <img alt="Lighthouse" class="lighthouse" src={Lighthouse}></img>
+        <div class="background"></div>
+        <div class="hero">
+          <h1>
+            {AboutContent.title}
+          </h1>
+          <p>
+            {AboutContent.paragraph1}
+          </p>
+          <p>
+            {AboutContent.paragraph2}
+          </p>
+          <p>
+            {AboutContent.paragraph3}
+          </p>
         </div>
-      );
-    }  
+      </div>
+    );
+  }
 }
-    
-  
-export default About;
 
+export default About;
 
 // <div>
 //   <h1>About Us Page</h1>
@@ -53,5 +37,3 @@ export default About;
 //     <button>Home</button>
 //   </Link>
 // </div>
-  
-
