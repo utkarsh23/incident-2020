3faa804058c30cabb112efc4f09cc28b46fe3381
Adharsh Kamath <adharshkamathr@gmail.com>
2020-01-31 16:36:13 +0530

    Events page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from "react-router-dom";
 import Home from "./Home";
 import Events from "./Events";
+import LitEvents from "./LitEvents";
 import Timeline from "./Timeline";
 import Contact from "./Contact";
 import Sponsors from "./Sponsors";
@@ -11,21 +16,64 @@ import Team from "./Team";
 import TrystWithTime from "./TrystWithTime";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/events" component={Events} />
-        <Route path="/timeline" component={Timeline} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/sponsors" component={Sponsors} />
-        <Route path="/about" component={About} />
-        <Route path="/icare" component={Icare} />
-        <Route path="/team" component={Team} />
-        <Route path="/tryst-with-time" component={TrystWithTime} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return ( <
+        BrowserRouter >
+        <
+        Switch >
+        <
+        Route exact path = "/"
+        component = {
+            Home
+        }
+        /> <
+        Route path = "/events"
+        component = {
+            Events
+        }
+        /> <
+        Route path = "/litevents"
+        component = {
+            LitEvents
+        }
+        /> <
+        Route path = "/timeline"
+        component = {
+            Timeline
+        }
+        /> <
+        Route path = "/contact"
+        component = {
+            Contact
+        }
+        /> <
+        Route path = "/sponsors"
+        component = {
+            Sponsors
+        }
+        /> <
+        Route path = "/about"
+        component = {
+            About
+        }
+        /> <
+        Route path = "/icare"
+        component = {
+            Icare
+        }
+        /> <
+        Route path = "/team"
+        component = {
+            Team
+        }
+        /> <
+        Route path = "/tryst-with-time"
+        component = {
+            TrystWithTime
+        }
+        /> <
+        /Switch> <
+        /BrowserRouter>
+    );
 }
 
 export default App;
